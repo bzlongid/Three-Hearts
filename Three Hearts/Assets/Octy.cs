544191fa950b3c0938439cc6fcf0da57b89210bc
Bethany Zeya Bucalan-Longid <55968638+bzlongid@users.noreply.github.com>
2022-11-18 22:06:54 -0800

    game scene set up

---
+++ Three Hearts/Assets/Octy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Octy : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    // just testing!
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetBool("willThrow", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("willThrow", false);
+        }
+    }
+}
---
+++ Three Hearts/Assets/Scripts/Controller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private CharacterController controller;
+    private float playerSpeed = 2.0f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+    }
+}
